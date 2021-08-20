00cc3002f49c592053850c6c7a93445e91ec2705
randomplayer007 <84766541+randomplayer007@users.noreply.github.com>
2021-08-20 20:28:38 +0530

    done

--- Program-2.c
+++ Program-2.c
@@ -11,8 +11,8 @@ struct student
 int main()  
 {  
   struct student s1,s2,s3;  
-  printf("Enter the  marks of student 1,2 and 3");  
-// Read the marks of three students
+  printf("Enter the  marks of student 1,2 and 3"); 
+ scanf("%d %d %d",&s1.marks,&s2.marks,&s3.marks); 
  
    printf("%d %d %d",s1.marks,s2.marks,s3.marks); 
   return 0;
